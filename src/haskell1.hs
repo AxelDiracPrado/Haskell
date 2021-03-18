fa396feb8afc7e44a6ae04699f8f71bf379a80a3
Axel Prado <axelpradoescamilla@ciencias.unam.mx>
2021-03-17 19:41:45 -0600

    Agrega funciones con tipos

--- src/haskell1.hs
+++ src/haskell1.hs
@@ -11,4 +11,15 @@ boomBang xs = [if x < 10 then "Boom" else "Bang" | x <- xs, odd x]
 
 length' xs = sum [1 |  _ <- xs]
 
+removeNotUpperCase :: String -> String
 removeNotUpperCase str = [c | c <- str, elem c ['A'..'Z']]
+
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+
+circunferencia :: Float -> Float
+circunferencia r = 2*pi*r
+
+circunferencia' :: Double -> Double
+circunferencia' r = 2*pi*r
+ 
